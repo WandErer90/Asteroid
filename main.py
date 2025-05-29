949b49da1019725c93aeb54c4116db3f79b7d616
WandErer90 <131876293+WandErer90@users.noreply.github.com>
2025-05-29 09:05:38 +0100

    added player class

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,7 +18,10 @@ def main():
                 return
 
         screen.fill("black")
-        pygame.display.flip()	
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000	
 		
 
 if __name__ == "__main__":
